bb10322bc8b60ede86e35cdabdddb3a9098b4130
Thanathip Limna <boatkrap@gmail.com>
2017-12-17 15:26:18 +0700

    modify api

--- principal/views/api/__init__.py
+++ principal/views/api/__init__.py
@@ -1,12 +1,11 @@
-from flask import Blueprint, render_template, g, request, jsonify
-from flask_login import login_required
-
+from flask import Blueprint, request, jsonify
 
 from principal.renderers import render_json
 from principal.oauth import oauth2
+from principal import models
+
+
 module = Blueprint('api', __name__, url_prefix='/api')
-
-
 
 
 @module.route('/email')
@@ -26,10 +25,12 @@ def me():
                             last_name=user.last_name,
                             email=user.email,
                             username=user.username,
+                            roles=user.roles,
                             last_update=user.updated_date))
+
 
 @module.route('/user/<username>')
 @oauth2.require_oauth('email')
 def user(username):
-    user = User.query.filter_by(username=username).first()
+    user = models.User.objects(username=username).first()
     return jsonify(email=user.email, username=user.username)
